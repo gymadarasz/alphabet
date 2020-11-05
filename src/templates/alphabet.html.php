4f617dbc3bc698407181e2bbe95421413b198600
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-11-05 18:35:53 +0100

    Hint as table

--- src/templates/alphabet.html.php
+++ src/templates/alphabet.html.php
@@ -2,6 +2,7 @@
 
 <div class="row">
     <div class="col text-center">
+        <br>
         <h1>Alphabet</h1>
         <p>
             Send and decode your secret message with this Alphabet message converter.<br>
@@ -46,8 +47,77 @@
 
 <div class="row">
     <div class="col text-center">
+        <br>
         If you are not sure how it works here is a hint:<br>
-        <img style="max-width:90%" src="https://i.pinimg.com/736x/94/ba/9d/94ba9d2ec86d4047ff20bda7d11e32dd.jpg">
+        <br>
+        <table class="table">
+            <tr>
+                <td><strong>A</strong></td>
+                <td><strong>B</strong></td>
+                <td><strong>C</strong></td>
+                <td><strong>D</strong></td>
+                <td><strong>E</strong></td>
+                <td><strong>F</strong></td>
+                <td><strong>H</strong></td>
+                <td><strong>G</strong></td>
+                <td><strong>I</strong></td>
+                <td>J</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+            </tr>
+            <tr><td colspan="10"></td></tr>
+            <tr>
+                <td><strong>K</strong></td>
+                <td><strong>L</strong></td>
+                <td><strong>M</strong></td>
+                <td><strong>N</strong></td>
+                <td><strong>O</strong></td>
+                <td><strong>P</strong></td>
+                <td><strong>Q</strong></td>
+                <td><strong>R</strong></td>
+                <td><strong>S</strong></td>
+                <td>T</td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+            </tr>
+            <tr><td colspan="10"></td></tr>
+            <tr>
+                <td><strong>U</strong></td>
+                <td><strong>V</strong></td>
+                <td><strong>W</strong></td>
+                <td><strong>X</strong></td>
+                <td><strong>Y</strong></td>
+                <td><strong>Z</strong></td>
+            </tr>
+            <tr>
+                <td>21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+                <td>25</td>
+                <td>26</td>
+            </tr>
+        </table>
     </div>
 </div>
 
