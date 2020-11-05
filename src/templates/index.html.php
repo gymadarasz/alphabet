c43958bcab06dc0093d9fe1d2fb104966c40f56a
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-11-05 14:52:49 +0100

    Change analytics code

--- src/templates/index.html.php
+++ src/templates/index.html.php
@@ -2,13 +2,13 @@
 <html lang="en">
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z3KNFC0RXM"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XLSCE3XY9L"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'G-Z3KNFC0RXM');
+            gtag('config', 'G-XLSCE3XY9L');
         </script>
 
         <!-- TODO add to a separated template -->
