5b2902be677f16c9d3e8a34fa915a556d706110a
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-11-05 14:03:09 +0100

    Fixing broken test

--- tests/AlphabetTest.php
+++ tests/AlphabetTest.php
@@ -18,7 +18,9 @@ class AlphabetTest extends AppTest
         $contents3 = $this->tester->get('?alphabet=8%2F5%2F12%2F12%2F15%2C+8%2F15%2F23+1%2F18%2F5+25%2F15%2F21%3F+16%2F12%2F5%2F1%2F19%2F5+21%2F19%2F5+16%2F21%2F14%2F3%2F20%2F21%2F1%2F20%2F9%2F15%2F14+9%2F14+20%2F8%2F9%2F19+12%2F15%2F14%2F7+20%2F5%2F24%2F20+12%2F9%2F11%2F5+20%2F8%2F9%2F19+%3A%29');
         
         $this->tester->assertEquals($contents1, $contents2);
-        $this->tester->assertEquals($contents1, $contents3);
+        $this->tester->assertNotEquals($contents1, $contents3);
+        $this->tester->assertContains('8/5/12/12/15, 8/15/23 1/18/5 25/15/21? 16/12/5/1/19/5 21/19/5 16/21/14/3/20/21/1/20/9/15/14 9/14 20/8/9/19 12/15/14/7 20/5/24/20 12/9/11/5 20/8/9/19 :)', $contents2);
+        $this->tester->assertContains('HELLO, HOW ARE YOU? PLEASE USE PUNCTUATION IN THIS LONG TEXT LIKE THIS :)', $contents3);
 
 
         $contents = $this->tester->get('?alphabet=9+12%2F15%2F22%2F5+25%2F15%2F21+20%2F15%2F15');
